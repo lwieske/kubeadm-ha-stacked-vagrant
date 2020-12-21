d84e8ea9c56ab5de7be56697a8a81cf70399d481
Lothar Wieske <lothar.wieske@gmail.com>
2020-12-21 14:50:51 +0100

    Update Kubernetes 1.20.1

--- rec.sh
+++ rec.sh
@@ -8,6 +8,6 @@ asciinema rec -y -c "bash -x run.sh" demo.cast
 
 asciicast2gif demo.cast demo.gif
 
-gifsicle --colors 4 --resize 800x600 demo.gif >demo800x600.gif
+gifsicle --colors 8 --resize 800x600 demo.gif >demo800x600.gif
 
 rm -f demo.{cast,gif}
